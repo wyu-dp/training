6cd122bceb87403f9ec574fea01105f6b7e3bfd2
HashiCorp Training <training@hashicorp.com>
2017-06-29 03:34:29 +0000

    Initial commit

---
+++ main.tf
@@ -0,0 +1,73 @@
+#
+# DO NOT DELETE THESE LINES!
+#
+# Your AMI ID is:
+#
+#     ami-2378f540
+#
+# Your subnet ID is:
+#
+#     subnet-fdf3c88b
+#
+# Your security group ID is:
+#
+#     sg-67393600
+#
+# Your Identity is:
+#
+#     datapipe-primate
+#
+
+terraform {
+  backend "atlas" {
+    name = "wyu/training"
+  }
+}
+
+provider "aws" {
+  access_key = "${var.aws_access_key}"
+  secret_key = "${var.aws_secret_key}"
+  region     = "${var.aws_region}"
+}
+
+resource "aws_instance" "web" {
+  ami                    = "ami-2378f540"
+  instance_type          = "t2.micro"
+  subnet_id              = "subnet-fdf3c88b"
+  vpc_security_group_ids = ["sg-67393600"]
+  count                  = 2
+
+  tags {
+    "Identity" = "datapipe-primate"
+    "Name"     = "WEB-${count.index+1}/${var.num_webs}}"
+  }
+}
+
+variable "num_webs" {
+  default = 2
+}
+
+variable "aws_access_key" {
+  type = "string"
+}
+
+variable "aws_secret_key" {
+  type = "string"
+}
+
+variable "aws_region" {
+  type    = "string"
+  default = "ap-southeast-1"
+}
+
+output "Name_Tag" {
+  value = "${aws_instance.web.*.tags.Name}"
+}
+
+output "public_ip" {
+  value = "${aws_instance.web.*.public_ip}"
+}
+
+output "public_dns" {
+  value = "${aws_instance.web.*.public_dns}"
+}
